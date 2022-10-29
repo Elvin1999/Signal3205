108c3eba08008fed6ca4ba28ba264467b2b34222
Elvin Camalzade <e.camalzade@itstep.edu.az>
2022-10-29 13:40:55 +0400

    Add project files.

---
+++ Signal/SignalRChat.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Signal
+{
+    public class SignalRChat:Hub
+    {
+        public async Task SendMessage(string user,string message)
+        {
+            await Clients.Others.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            
+            string info = " connected successfully";
+            await Clients.Others.SendAsync("Connect", info);
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            string info = $"someone disconnect";
+            return Clients.Others.SendAsync("DisConnect", info);
+        }
+    }
+}
---
+++ Signal/wwwroot/js/chat.js
@@ -0,0 +1,54 @@
+﻿"use strict"
+
+
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+document.getElementById("sendButton").disable = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+
+    document.getElementById("messagesList").appendChild(li);
+
+    li.textContent = `${user} says ${message}`;
+
+});
+
+connection.on("Connect", function (info) {
+
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    li.innerHTML = `<span style='color:#00bfff;' > ${info}</span>`;
+
+});
+
+
+connection.on("DisConnect", function (info) {
+
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    li.innerHTML = `<span style='color:red;' > ${info}</span>`;
+
+});
+
+
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+
+    connection.invoke("SendMessage", user, message)
+        .catch(function (err) {
+            return console.error(err.toString());
+        });
+
+    event.preventDefault();
+});
